a72006fc7ec8406b397fe67cd98ba8bf472a945b
Felipe <fmchora@gmail.com>
2016-10-16 18:14:10 -0600

    database

--- php-getting-started/web/database.php
+++ php-getting-started/web/database.php
@@ -19,7 +19,60 @@ $dbPassword = $dbopts["pass"];
 $dbName = ltrim($dbopts["path"],'/');
 
 print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+?>
 
+<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="../CSS/survey.css">
+    <title>Felipe Chora's Assignments</title>
+
+</head>
+
+<body>
+
+<div id="big_wraper">
+    <nav id="top_manu">
+
+        <ul>
+
+            <li> <a href="assignments.html "> CS 313 Assignments</a></li>
+            <li> <a href="https://byui.brightspace.com/d2l/home/136525" target="_blank"> I Learn</a></li>
+
+        </ul>
+
+    </nav>
+
+
+    <div id="main"> 
+	
+        <h1>Pantry tracker</h1>
+
+
+<?php
+
+// default Heroku Postgres configuration URL
+$dbUrl = getenv('DATABASE_URL');
+
+if (empty($dbUrl)) {
+ // example localhost configuration URL with postgres username and a database called cs313db
+ $dbUrl = "postgres://postgres:password@localhost:5432/cs313db";
+}
+
+$dbopts = parse_url($dbUrl);
+
+print "<p>$dbUrl</p>\n\n";
+
+$dbHost = $dbopts["host"]; 
+$dbPort = $dbopts["port"]; 
+$dbUser = $dbopts["user"]; 
+$dbPassword = $dbopts["pass"];
+$dbName = ltrim($dbopts["path"],'/');
+
+print "<p>pgsql:host=$dbHost;port=$dbPort;dbname=$dbName</p>\n\n";
+        
 try {
  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 }
@@ -31,9 +84,23 @@ catch (PDOException $ex) {
 foreach ($db->query('SELECT * FROM person') as $row)
 {
  echo '<p>';
- echo '<strong>' . $row['id'] . ' ' . $row['name'] . ' ' . $row['lastname'];
+ echo '<strong>' . 'User: ' . ' ' . $row['id'] . ' ' . $row['name'] . ' ' . $row['lastname'];
  echo '</strong>';
  echo '</p>';
+ echo '<p>Items:</p>';
+    foreach ($db->query('SELECT * FROM items') as $row)
+    {
+     echo '<li>';
+     echo '<strong>' . '    ' . $row['name'] . ' ' . $row['quantity'];
+     echo '</strong>';
+     echo '</li>';
+    }
 }
 
 ?>
+        
+<</div>
+	
+	</div>
+</body>
+</html>
